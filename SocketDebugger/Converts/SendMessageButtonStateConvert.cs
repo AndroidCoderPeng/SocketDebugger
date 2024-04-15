9f54488bc33170992f1feef09f55277d921d2347
pengxianhong <290677893@qq.com>
2024-04-15 09:46:55 +0800

    联动循环发送勾选和发送按钮关系

---
+++ SocketDebugger/Converts/SendMessageButtonStateConvert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SocketDebugger.Converts
+{
+    internal class SendMessageButtonStateConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.Assert(value != null, nameof(value) + " != null");
+            var isChecked = (bool)value;
+            return !isChecked;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
