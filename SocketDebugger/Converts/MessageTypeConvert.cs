1de623c327d93b73f96f28325167dfe1323f7145
pengxianhong <290677893@qq.com>
2024-09-12 09:13:29 +0800

    解决RadioButton绑定问题

--- SocketDebugger/Converts/MessageTypeConvert.cs
+++ SocketDebugger/Converts/MessageTypeConvert.cs
@@ -4,20 +4,16 @@ using System.Windows.Data;
 
 namespace SocketDebugger.Converts
 {
-    //TODO 不生效
     public class MessageTypeConvert : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            Console.WriteLine(value);
-            var equals = value == null || ReferenceEquals(value, "文本");
-            Console.WriteLine(equals);
-            return equals;
+            return value != null && value.ToString() == (string)parameter;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return null;
+            return value != null && value.Equals(true) ? parameter : Binding.DoNothing;
         }
     }
 }
